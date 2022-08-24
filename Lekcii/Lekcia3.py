226e5c85c187bd857bf2db9c810d82212fb4bb66
Biovstu <biovstu@gmail.com>
2022-08-24 20:17:19 +0300

    Лекция 4 добавилась

---
+++ Lekcii/Lekcia3.py
@@ -0,0 +1,17 @@
+# spis = [1,2,3,5,8,13,23,38]
+
+# list = [(spis[i], spis[i] ** 2) for i in range(len(spis)) if spis[i] % 2 == 0]
+# print(list)
+
+# data = '1 2 3 5 8 15 23 38'
+# numbers = []
+# while data != '':
+#     space_pos = data.index(' ')
+#     numbers.append(int(data[:space_pos]))
+#     data = data[space_pos+1:]
+# out = []
+# for e in numbers:
+#     if not e % 2:
+#         out.append((e,e **2))
+# print(out)
+
