6b4c4c6079cc995b5857e4fc45c5d5cf700ad84e
Biovstu <biovstu@gmail.com>
2022-08-28 22:39:54 +0300

    Решил задачу 5.3

--- Domashki/Domashka5.py
+++ Domashki/Domashka5.py
@@ -1,4 +1,5 @@
-# 1. Напишите программу, удаляющую из текста все слова, содержащие "абв".
+# Задача 5.1
+# Напишите программу, удаляющую из текста все слова, содержащие "абв".
 #     Входные и выходные данные хранятся в отдельных текстовых файлах.
 
 # with open('Domashki\input51.txt', mode='r', encoding='utf-8') as f:
@@ -11,7 +12,8 @@
 # with open('Domashki\output51.txt', mode='w', encoding='utf-8') as f:
 #     f.write(a)
 
-# 2. Создайте программу для игры с конфетами человек против человека.
+# Задача 5.2
+# Создайте программу для игры с конфетами человек против человека.
 #     Условие задачи: На столе лежит 2021 конфета. Играют два игрока делая ход друг после друга.
 #     Первый ход определяется жеребьёвкой. За один ход можно забрать не более чем 28 конфет. 
 #     Все конфеты оппонента достаются сделавшему последний ход. 
@@ -78,11 +80,50 @@
 # else:
 #     print('Ты победил....\nВ этот раз...')
 
-# 3. Создайте программу для игры в "Крестики-нолики".
+# Задача 5.3
+# Создайте программу для игры в "Крестики-нолики".
+
+# осталось добавить защиту от дурака и контроль дубля ходов
+# def prnt_tablo(tablo):
+#     print('+' + '-' * 11 + '+')
+#     print('| ' + tablo[0][0] + ' | ' + tablo[0][1] + ' | ' + tablo[0][2] + ' |')
+#     print('+' + '-' * 11 + '+')
+#     print('| ' + tablo[1][0] + ' | ' + tablo[1][1] + ' | ' + tablo[1][2] + ' |')
+#     print('+' + '-' * 11 + '+')
+#     print('| ' + tablo[2][0] + ' | ' + tablo[2][1] + ' | ' + tablo[2][2] + ' |')
+#     print('+' + '-' * 11 + '+')
 
 
+# from os import system
+# system('cls')
 
+# krest_nol = [[' ',' ',' '],[' ',' ',' '],[' ',' ',' ']]
+# hod_x = False
+# ne_pobeda = True
+# hody = []# защита от повтора ходов
+# while ne_pobeda:
+#     system('cls')
+#     prnt_tablo(krest_nol)
+#     hod_x = not hod_x
+#     if hod_x:
+#         mask = 'X'
+#     else:
+#         mask = '0'
+#     strok_stolbec = list(map(lambda x: int(x) - 1, list(input(f'Выберите ячейку для {mask} (строка столбец):\n').split())))
+#     krest_nol[strok_stolbec[0]][strok_stolbec[1]] = mask
+#     if krest_nol[strok_stolbec[0]][0] == mask and krest_nol[strok_stolbec[0]][1] == mask and krest_nol[strok_stolbec[0]][2] == mask:
+#         ne_pobeda = not ne_pobeda
+#     elif krest_nol[0][strok_stolbec[1]] == mask and krest_nol[1][strok_stolbec[1]] == mask and krest_nol[2][strok_stolbec[1]] == mask:
+#         ne_pobeda = not ne_pobeda
+#     elif krest_nol[0][0] == mask and krest_nol[1][1] == mask and krest_nol[2][2] == mask:
+#         ne_pobeda = not ne_pobeda
+#     elif krest_nol[0][2] == mask and krest_nol[1][1] == mask and krest_nol[2][0] == mask:
+#         ne_pobeda = not ne_pobeda
+# system('cls')
+# prnt_tablo(krest_nol)
+# print('Победу одержал',mask)
 
+# Задача 5.4
 # 4. Реализуйте RLE алгоритм: реализуйте модуль сжатия и восстановления данных.
 #     Входные и выходные данные хранятся в отдельных текстовых файлах.
 
