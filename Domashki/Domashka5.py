33d91abda3cbaf384a1054a03a00d49075c306c6
BioVstu <biovstu@gmail.com>
2022-09-05 12:17:48 +0300

    Протсо тест

--- Domashki/Domashka5.py
+++ Domashki/Domashka5.py
@@ -105,54 +105,54 @@
 # Создайте программу для игры в "Крестики-нолики".
 
 # осталось добавить защиту от дурака
-# def prnt_tablo(tablo):
-#     print('+' + '-' * 11 + '+')
-#     print('| ' + tablo[0][0] + ' | ' + tablo[0][1] + ' | ' + tablo[0][2] + ' |')
-#     print('+' + '-' * 11 + '+')
-#     print('| ' + tablo[1][0] + ' | ' + tablo[1][1] + ' | ' + tablo[1][2] + ' |')
-#     print('+' + '-' * 11 + '+')
-#     print('| ' + tablo[2][0] + ' | ' + tablo[2][1] + ' | ' + tablo[2][2] + ' |')
-#     print('+' + '-' * 11 + '+')
+def prnt_tablo(tablo):
+    print('+' + '-' * 11 + '+')
+    print('| ' + tablo[0][0] + ' | ' + tablo[0][1] + ' | ' + tablo[0][2] + ' |')
+    print('+' + '-' * 11 + '+')
+    print('| ' + tablo[1][0] + ' | ' + tablo[1][1] + ' | ' + tablo[1][2] + ' |')
+    print('+' + '-' * 11 + '+')
+    print('| ' + tablo[2][0] + ' | ' + tablo[2][1] + ' | ' + tablo[2][2] + ' |')
+    print('+' + '-' * 11 + '+')
 
 
-# from os import system
-# system('cls')
+from os import system
+system('cls')
 
-# krest_nol = [[' ',' ',' '],[' ',' ',' '],[' ',' ',' ']]
-# hod_x = False
-# ne_pobeda = True
-# hody = []
-# while ne_pobeda and len(hody) < 9:
-#     system('cls')
-#     prnt_tablo(krest_nol)
-#     hod_x = not hod_x
-#     if hod_x:
-#         mask = 'X'
-#     else:
-#         mask = '0'
-#     wrong = True
-#     while wrong:
-#         strok_stolbec = list(map(lambda x: int(x) - 1, list(input(f'Выберите ячейку для {mask} (строка столбец):\n').split())))
-#         if strok_stolbec not in hody:
-#             wrong = False
-#             hody.append(strok_stolbec)
-#         else:
-#             print('Такой ход уже был, попробуйте снова...')
-#     krest_nol[strok_stolbec[0]][strok_stolbec[1]] = mask
-#     if krest_nol[strok_stolbec[0]][0] == mask and krest_nol[strok_stolbec[0]][1] == mask and krest_nol[strok_stolbec[0]][2] == mask:
-#         ne_pobeda = False
-#     elif krest_nol[0][strok_stolbec[1]] == mask and krest_nol[1][strok_stolbec[1]] == mask and krest_nol[2][strok_stolbec[1]] == mask:
-#         ne_pobeda = False
-#     elif krest_nol[0][0] == mask and krest_nol[1][1] == mask and krest_nol[2][2] == mask:
-#         ne_pobeda = False
-#     elif krest_nol[0][2] == mask and krest_nol[1][1] == mask and krest_nol[2][0] == mask:
-#         ne_pobeda = False
-# system('cls')
-# prnt_tablo(krest_nol)
-# if len(hody) < 9:
-#     print('Победу одержал',mask)
-# else:
-#     print('Ничья')
+krest_nol = [[' ',' ',' '],[' ',' ',' '],[' ',' ',' ']]
+hod_x = False
+ne_pobeda = True
+hody = []
+while ne_pobeda and len(hody) < 9:
+    system('cls')
+    prnt_tablo(krest_nol)
+    hod_x = not hod_x
+    if hod_x:
+        mask = 'X'
+    else:
+        mask = '0'
+    wrong = True
+    while wrong:
+        strok_stolbec = list(map(lambda x: int(x) - 1, list(input(f'Выберите ячейку для {mask} (строка столбец):\n').split())))
+        if strok_stolbec not in hody:
+            wrong = False
+            hody.append(strok_stolbec)
+        else:
+            print('Такой ход уже был, попробуйте снова...')
+    krest_nol[strok_stolbec[0]][strok_stolbec[1]] = mask
+    if krest_nol[strok_stolbec[0]][0] == mask and krest_nol[strok_stolbec[0]][1] == mask and krest_nol[strok_stolbec[0]][2] == mask:
+        ne_pobeda = False
+    elif krest_nol[0][strok_stolbec[1]] == mask and krest_nol[1][strok_stolbec[1]] == mask and krest_nol[2][strok_stolbec[1]] == mask:
+        ne_pobeda = False
+    elif krest_nol[0][0] == mask and krest_nol[1][1] == mask and krest_nol[2][2] == mask:
+        ne_pobeda = False
+    elif krest_nol[0][2] == mask and krest_nol[1][1] == mask and krest_nol[2][0] == mask:
+        ne_pobeda = False
+system('cls')
+prnt_tablo(krest_nol)
+if len(hody) < 9:
+    print('Победу одержал',mask)
+else:
+    print('Ничья')
 
 # Задача 5.4
 # 4. Реализуйте RLE алгоритм: реализуйте модуль сжатия и восстановления данных.
