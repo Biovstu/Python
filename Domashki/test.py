7dc8b33b6f48a2f69e17532343b28d9de4ce01c1
Biovstu <biovstu@gmail.com>
2022-09-17 12:20:11 +0300

    proverka

--- Domashki/test.py
+++ Domashki/test.py
@@ -1,14 +1,23 @@
-a_r = 2.5
-a_i = 2
+# a_r = 2.5
+# a_i = 2
+#
+# a = complex(a_r, a_i)
+# b = complex(1, 3)
+# print('a - b =', a - b)
+#
+# def print_complex(a: complex):
+#     if a.imag == 0:
+#         return int(a.real) if a.real.is_integer() else a.real
+#     else:
+#         return a
+#
+# print('a - b =', print_complex(a - b))
 
-a = complex(a_r, a_i)
-b = complex(1, 3)
-print('a - b =', a - b)
+a = float('-inf')
+print(a)
+b = float('+inf')
+print(b)
 
-def print_complex(a: complex):
-    if a.imag == 0:
-        return int(a.real) if a.real.is_integer() else a.real
-    else:
-        return a
-
-print('a - b =', print_complex(a - b))
+c = (a, b)
+print(c)
+print((c[0] + c[1]) / 2)
